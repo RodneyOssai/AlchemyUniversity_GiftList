61e85b66739b5cab4c90d0f75bb4e2403247c39c
Rodney Ossai <ossai2007@gmail.com>
2023-05-29 13:16:18 +0100

    Inital Commit

--- client/index.js
+++ client/index.js
@@ -6,9 +6,18 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const name = "Lillie Feest";
+  // Create the Merkle tree
+  const merkleTree = new MerkleTree(niceList);
+  // Get the index of the name in the list
+  const index = niceList.findIndex(n => n === name);
+
+  // Get the proof for the name
+  const proof = merkleTree.getProof(index);
+
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name, proof
   });
 
   console.log({ gift });
